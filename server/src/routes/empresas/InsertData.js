f36e3efee2511c0ce97a4c6cb3e68a63b5ae4f3c
Bruno Carmo <Bruno.reis.carmo@gmail.com>
2023-11-01 08:52:00 -0300

    Backend: Insert dados do MySQL => res.json #2 (#5)

--- server/src/routes/empresas/InsertData.js
+++ server/src/routes/empresas/InsertData.js
@@ -5,23 +5,34 @@ const connection = require("../../config");
 router.post("/empresas", (req, res) => {
   const { cnpj, razao_social, nome_fantasia, status } = req.body;
 
-  if (!cnpj || !razao_social || !nome_fantasia || !status) {
-    res.status(400).json({ message: "Todos são necessários." });
+  if (!cnpj || !razao_social || !nome_fantasia || status === undefined) {
+    res
+      .status(400)
+      .json({ Mensagem: "Todos são necessários e necessitam ser válidos." });
     return;
   }
 
-  const insertQuery = "INSERT INTO empresas_data (cnpj, razao_social, nome_fantasia, status) VALUES (?, ?, ?, ?)";
+  const insertQuery =
+    "INSERT INTO empresas_data (cnpj, razao_social, nome_fantasia, status) VALUES (?, ?, ?, ?)";
 
-  connection.query(insertQuery, [cnpj, razao_social, nome_fantasia, status], (error, results) => {
-    if (error) {
-      console.error("Erro ao adicionar dados:", error);
-      res.status(500).json({ message: "Error adicionando dados no Banco de Dados." });
-      return;
+  connection.query(
+    insertQuery,
+    [cnpj, razao_social, nome_fantasia, status],
+    (error, results) => {
+      if (error) {
+        console.error("Erro ao adicionar dados:", error);
+        res
+          .status(500)
+          .json({ Mensagem: "Erro ao adicionar dados no Banco de Dados." });
+        return;
+      }
+
+      console.log("Empresa cadastrada com sucesso:", results);
+      res.json({
+        Mensagem: "Empresa cadastrada com sucesso no banco de dados.",
+      });
     }
-
-    console.log("Empresa cadastrada com sucesso:", results);
-    res.json({ message: "Empresa cadastrada com sucesso ao banco de dados." });
-  });
+  );
 });
 
 module.exports = router;

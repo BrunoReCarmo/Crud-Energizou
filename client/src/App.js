357f7896fca771574aa24e796cc80a2179b841dc
bruno.reis.carmo@gmail.com <bruno.reis.carmo@gmail.com>
2023-11-01 12:41:22 -0300

    Frontend: Header (#12)

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Flex, Box } from "@chakra-ui/react";
 import { Home, Cadastro } from "./pages";
-import { Sidebar } from "./components";
+import { Sidebar, Header } from "./components";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Flex h="100vh" flexDirection="column" className="bg-slate-800">
-        <h1>aqui será o header</h1>
+        <Header />
         <Flex w="100%" my="6" maxW={1200} mx="auto">
           <Sidebar />
           <Box w="100%">

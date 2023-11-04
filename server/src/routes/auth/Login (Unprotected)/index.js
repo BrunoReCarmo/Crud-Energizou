e6a5f22055ccd6d9827e53017203e7b1144fc60f
Bruno Reis <bruno.reis.carmo@gmail.com>
2023-11-04 10:06:20 -0300

    Backend: JWT com infos 'nome' do usuário authenticado (#41)

--- server/src/routes/auth/Login (Unprotected)/index.js
+++ server/src/routes/auth/Login (Unprotected)/index.js
@@ -14,10 +14,10 @@ LoginRoute.post("/api/login", (req, res) => {
         }
         if (data.length > 0) {
             const id = data[0].id;
-            const name = data[0].nome;
+            const nome = data[0].nome;
             const email = data[0].email;
             const contato = data[0].contato;
-            const token = jwt.sign({ id, name, email, contato }, jwt_secret_key, { expiresIn: '8h' });
+            const token = jwt.sign({ id, nome, email, contato }, jwt_secret_key, { expiresIn: '8h' });
             return res.json({ Login: true, token, data, message: "Success" });
         } else {
             return res.json({ message: "Fail" });
